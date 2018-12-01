bf7580eae0fb51a3a98485d0bfb43f0e2728177e
Viren Shah <vvs24@cornell.edu>
2018-12-01 13:11:46 -0500

    Added more sounds

--- src/mvp.py
+++ src/mvp.py
@@ -4,30 +4,30 @@ import nltk
 nltk.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer as sid
 from random import *
-import simpleaudio as sa                     
+import simpleaudio as sa
 
 
-def main():                              
+def main():
 	r = sr.Recognizer()
-	
+
 	### opens microphone and takes speech from human to convert to text
 	mic = sr.Microphone(2)
 	with mic as source:
 		r.adjust_for_ambient_noise(source)
 		print("\n\n\nYou may begin talking:\n\n\n")
 		audio = r.listen(source)
-	
+
 	try:
 		### parsing speech to text
 		spoken = r.recognize_google(audio)
 		print("The following text was said:\n\n" + spoken)
-		
+
 		### use basic NLTK sentiment analysis algo Vader to assess speech
 		senti_analyzer = sid().polarity_scores(spoken)['compound']
 		print ("On a -1 to 1 scale (< 0 is negative, > 0 is positive, = 0 is neutral), the text is: " + str(senti_analyzer))
 		#TODO: change this section to be more specific to perform more specific analysis
-		
-	        ### sound output
+
+    ### sound output
 		lead_folder = "/home/pi/r2-voice_recognition/R2FinalSounds/"
 		sounds = {"angry":"R2Angry.wav" , "good":"R2Good.wav" , "happy":"R2Happy.wav" , "neutral":"R2Neutral.wav", "sad":"R2Sad.wav"}
 		if (senti_analyzer < -0.5):
@@ -40,8 +40,7 @@ def main():
 			play_sound(lead_folder + sounds["happy"])
 		else:
 			play_sound(lead_folder + sounds["good"])
-	        
-                #TODO: change and add sounds for more sentiment (after new algorithm has been constructed)
+    #TODO: change and add sounds for more sentiment (after new algorithm has been constructed)
 
 	except sr.UnknownValueError:
 		print ("What are you saying?")
