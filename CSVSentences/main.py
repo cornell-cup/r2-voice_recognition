8ffb01450f9da30d776d8a91548136ec3681cf51
Varun <vm324@cornell.edu>
2019-04-13 16:11:39 -0400

    test robot continued

--- CSVSentences/main.py
+++ CSVSentences/main.py
@@ -1,20 +1,9 @@
-# import respective packages
+import csv
 import sys
-#import speech_recognition as sr
-#import pyaudio
 import nltk
-nltk.download('vader_lexicon')
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer as sid
-#from random import *
 import simpleaudio as sa
 import json
-#import client
-#import socket
-#import json
-#import time
-#from threading import Thread
 from watson_developer_cloud import NaturalLanguageUnderstandingV1
 from watson_developer_cloud.natural_language_understanding_v1 \
     import Features, EntitiesOptions, KeywordsOptions, SentimentOptions
@@ -23,21 +12,36 @@ naturalLanguageUnderstanding = NaturalLanguageUnderstandingV1(
 version='2018-11-16',
 iam_apikey='ZpNv1kcHqUvvzupBoxNRa-PvNKf-vbLnL6QLjBZTvHmr')
 
-import csv
-with open("sentences.txt", newline='', encoding='utf-8') as f:
+with open("C:\PythonProjects\\r2-voice_recognition\CSVSentences\sentences.txt", newline='', encoding='utf-8') as f:
     reader = csv.reader(f, delimiter='\t')
     csvData = []
     for row in reader:
         spoken = row[0]
         sentiment_value = sid().polarity_scores(spoken)['compound']
         print(sentiment_value)
-        row.append(sentiment_value)
         print(row)
         print(spoken)
+        row.append(sentiment_value)
+        try: 
+         response = naturalLanguageUnderstanding.analyze(
+         text = spoken,
+         features = Features(
+         sentiment = SentimentOptions(document=None, targets = None))).get_result()
+         
+         parsed_json = json.loads(json.dumps(response, indent=2))
+         sentiment = parsed_json['sentiment']
+         document = sentiment['document']
+         score = document['score']
+         sentiment_value = float(score)
+
+        except:
+         sentiment_value = -2
+        
+        row.append(sentiment_value)
         csvData.append(row)
    
 with open('sentences.csv', 'w', newline='') as csvFile:
     writer = csv.writer(csvFile)
-    headers = ["Sentences", "Sentiment Value", "NLTK Prediction"]
+    headers = ["Sentences", "Sentiment Value", "NLTK Prediction", "Watson Prediction"]
     writer.writerow(headers)
     writer.writerows(csvData)
